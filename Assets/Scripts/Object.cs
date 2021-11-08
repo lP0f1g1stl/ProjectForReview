cb075a0373748f837bf4d0e2cf29b99317e5fd54
lP0f1g1stl <64088803+lP0f1g1stl@users.noreply.github.com>
2021-11-08 19:33:16 +0300

    test commit 2

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    private int _damage;
+    private int _points;
+
+    private bool _isActive;
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _spriteRenderer;
+    private ParticleSystem _particleSystem;
+
+    public delegate void OnClick(int points);
+    public static event OnClick IsClicked;
+
+    private void Awake()
+    {
+        _rb = GetComponent <Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void SetColor(Color ballColor) 
+    {
+        _spriteRenderer.color = ballColor;
+        _particleSystem.startColor = ballColor;
+    }
+
+    public int GetDamage() 
+    {
+        return _damage;
+    }
+    public void SetVelocity(Vector2 velocity) 
+    {
+        _rb.velocity = velocity;
+    }
+    public void SetBallData(Color color, int damage,int points) 
+    {
+        _damage = damage;
+        _points = points;
+        SetColor(color);
+        IsActive(true);
+    }
+
+    public void SetBallPosition(float rand)
+    {
+        gameObject.transform.position = new Vector2(rand, 3);
+    }
+
+    public void StartParticlesAndSoundsOnDestroy() 
+    {
+        _particleSystem.Play();
+        _spriteRenderer.color = Color.clear;
+        _rb.velocity = new Vector2(0, 0);
+        IsClicked?.Invoke(0);
+    }
+    private void IsActive(bool isActive) 
+    {
+        _isActive = isActive;
+    }
+    private void OnMouseDown()
+    {
+        if (Time.timeScale != 0 && _isActive)
+        {
+            IsClicked?.Invoke(_points);
+            IsActive(false);
+            StartParticlesAndSoundsOnDestroy();
+            StartCoroutine(Disable());
+        }
+    }
+
+    private IEnumerator Disable() 
+    {
+        yield return new WaitForSeconds(2);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int _playerHealth;
+    [SerializeField] private Text _playerHealthText;
+
+    public delegate void GameIsActive(bool isActive);
+    public static event GameIsActive IsActive;
+
+    private void Start()
+    {
+        UpdatePlayerHealthText();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Object>()) 
+        {
+            _playerHealth -= collision.GetComponent<Object>().GetDamage();
+            collision.GetComponent<Object>().StartParticlesAndSoundsOnDestroy();
+            CheckPlayerHealth();
+            UpdatePlayerHealthText();
+        }
+    }
+
+    private void CheckPlayerHealth() 
+    {
+       if(_playerHealth <= 0) 
+        {
+            Time.timeScale = 0;
+            _playerHealth = 0;
+            IsActive?.Invoke(false);
+        }
+        UpdatePlayerHealthText();
+    }
+    private void UpdatePlayerHealthText() 
+    {
+        _playerHealthText.text = "HP: " + _playerHealth.ToString();
+    }
+}
